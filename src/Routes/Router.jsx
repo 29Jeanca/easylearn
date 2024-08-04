7941530083c1dc48ac31448fb5cf5ae689e4e03c
29Jeanca <jeancarlosbarberena29@gmail.com>
2024-08-04 00:54:59 -0600

    deploy medio funciona

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,10 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
-
+import Grammar from "../Pages/Grammar";
+import Vocabulary from "../Pages/Vocabulary";
+import Pronunciation from "../Pages/Pronunciation";
+import Conversation from "../Pages/Conversation";
 const router = createBrowserRouter([
     {
         path: '/easylearn',
         element: <Home/>
+    },
+    {
+        path: "/",
+        element: <Grammar/>
+    },
+    {
+        path: "/vocabulary",
+        element: <Vocabulary/>
+    },
+    {
+        path: "/pronunciation",
+        element: <Pronunciation/>
+    },
+    {
+        path: '/conversation',
+        element: <Conversation/>
     }
 ])
 export default router;
